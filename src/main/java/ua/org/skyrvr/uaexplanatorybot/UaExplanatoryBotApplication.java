585f7041413624e7d16e8705f248de1c979fda9c
Agienko Sergiy <agienko.sergiy@gmail.com>
2018-05-17 23:22:10 +0300

    Initial commit

---
+++ src/main/java/ua/org/skyrvr/uaexplanatorybot/UaExplanatoryBotApplication.java
@@ -0,0 +1,12 @@
+package ua.org.skyrvr.uaexplanatorybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaExplanatoryBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaExplanatoryBotApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/org/skyrvr/uaexplanatorybot/UaExplanatoryBotApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.org.skyrvr.uaexplanatorybot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UaExplanatoryBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
